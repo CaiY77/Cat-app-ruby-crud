3e55e195662ca9740d386bcdb4e686777e901a9f
Cai Yang <caih.yang@gmail.com>
2019-05-24 13:37:43 -0400

    cru

--- CatApp/app/controllers/cats_controller.rb
+++ CatApp/app/controllers/cats_controller.rb
@@ -1,9 +1,47 @@
 class CatsController < ApplicationController
+
+  before_action :get_cat, only: [:show,:edit,:update]
+
   def index
     @cats = Cat.all
   end
 
   def show
+  end
+
+  def new
+    @cat = Cat.new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to cat_path(@cat)
+    else
+     render :new
+    end
+
+  end
+
+  def edit
+  end
+
+  def update
+    if @cat.update(cat_params)
+      redirect_to cat_path(@cat)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def get_cat
     @cat = Cat.find(params[:id])
   end
+
+  def cat_params
+    params.require(:cat).permit(:name, :breed)
+  end
+
 end
